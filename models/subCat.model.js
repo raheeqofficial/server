bc658c24802063ca89ca895f224e1373fc78cd56
Muhammad Raheeq Gill <raheeqgillofficial@gmail.com>
2024-06-22 18:24:56 +0500

    Initial commit

---
+++ models/subCat.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const SubCatSchema = mongoose.Schema({
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        required: true
+    },
+    subCat: {
+        type: String,
+        required:  true
+    },
+});
+
+SubCatSchema.virtual('id').get(function (){
+    return this._id.toHexString();
+});
+SubCatSchema.set('toJSON', {
+    virtuals: true
+})
+
+exports.SubCat = mongoose.model("SubCat", SubCatSchema);
+exports.SubCatSchema = SubCatSchema
---
+++ routes/orders.js
@@ -0,0 +1,142 @@
+const { Cart } = require('../models/cart');
+const { Orders } = require('../models/orders');
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get(`/`, async (req, res) => {
+
+    try {
+    
+
+        const ordersList = await Orders.find(req.query)
+
+        if (!ordersList) {
+            res.status(500).json({ success: false })
+        }
+
+        return res.status(200).json(ordersList);
+
+    } catch (error) {
+        res.status(500).json({ success: false })
+    }
+
+
+});
+
+
+router.get('/:id', async (req, res) => {
+
+    const order = await Orders.findById(req.params.id);
+
+    if (!order) {
+        res.status(500).json({ message: 'The order with the given ID was not found.' })
+    }
+    return res.status(200).send(order);
+})
+
+router.get(`/get/count`, async (req, res) =>{
+    const orderCount = await Orders.countDocuments()
+
+    if(!orderCount) {
+        res.status(500).json({success: false})
+    } 
+    res.send({
+        orderCount: orderCount
+    });
+})
+
+
+
+router.post('/create', async (req, res) => {
+
+    let order = new Orders({
+        name: req.body.name,
+        phoneNumber: req.body.phoneNumber,
+        address: req.body.address,
+        pincode: req.body.pincode,
+        amount: req.body.amount,
+        email: req.body.email,
+        userid: req.body.userid,
+        products: req.body.products,
+    });
+
+
+
+    if (!order) {
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    }
+
+
+    order = await order.save();
+    // Clear the cart for the user
+    await Cart.deleteMany({ userId: req.body.userid });
+    res.status(201).json(order);
+
+});
+
+
+router.delete('/:id', async (req, res) => {
+
+    const deletedOrder = await Orders.findByIdAndDelete(req.params.id);
+
+    if (!deletedOrder) {
+        res.status(404).json({
+            message: 'Order not found!',
+            success: false
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        message: 'Order Deleted!'
+    })
+});
+
+
+router.put('/:id', async (req, res) => {
+
+    const order = await Orders.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            phoneNumber: req.body.phoneNumber,
+            address: req.body.address,
+            pincode: req.body.pincode,
+            amount: req.body.amount,
+            email: req.body.email,
+            userid: req.body.userid,
+            products: req.body.products,
+            status:req.body.status
+        },
+        { new: true }
+    )
+
+
+
+    if (!order) {
+        return res.status(500).json({
+            message: 'Order cannot be updated!',
+            success: false
+        })
+    }
+
+    res.send(order);
+
+})
+
+
+router.get('/totalRevenue', async(req, res) => {
+    const orders = await Orders.find();
+    const totalOrders = orders.length;
+    const totalRevenue = orders.reduce((acc, order) => acc + order.subTotal, 0)
+    res.send(totalRevenue)
+})
+
+
+module.exports = router;
+
